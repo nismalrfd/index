79dd26b32e4f7ba320176f1400e1343088e574ab
nismal <110675248+nismalrfd@users.noreply.github.com>
2026-07-09 16:04:58 +0530

    Delete mobile directory

--- mobile/recover.ps1
+++
@@ -1,2 +0,0 @@
-[System.IO.File]::Copy("C:\Users\nismal\Downloads\New folder (2)\New folder (3)\suite.html", "C:\Users\nismal\Downloads\New folder (2)\suite.html", $true)
-Write-Host "Recovered backup!"
